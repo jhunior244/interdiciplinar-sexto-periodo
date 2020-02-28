5c4c15305bf9cc196c0763a59b2be70778a837c6
jhuniormoreira2017 <jhuniormoreira2017@outlook.com>
2020-02-27 22:09:44 -0300

    tela desktop

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { rotas } from './rotas';
+import { configuracao } from './configuracao';
 import { TelaInicialComponent } from './tela/tela-inicial/tela-inicial.component';
+import { TelaListaDesktopComponent } from './tela/tela-lista-desktop/tela-lista-desktop.component';
 
 
 const routes: Routes = [
-  {path: rotas.rotaInicio, component: TelaInicialComponent},
+  { path: configuracao.rotaInicio, component: TelaInicialComponent },
+  { path: configuracao.rotaDesktop, component: TelaListaDesktopComponent },
 
 
-
-  {path: '**', component: TelaInicialComponent}
+  { path: '**', component: TelaInicialComponent }
 ];
 
 @NgModule({
